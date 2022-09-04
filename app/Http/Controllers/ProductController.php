821a22d14b7fd8d0800bb53a8cf54cd294865cf7
Jairo-Jr <jairo.huallpa1@gmail.com>
2022-09-03 23:01:17 -0500

    ruta para la obtencion de productos

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
@@ -13,10 +14,31 @@ class ProductController extends Controller
      */
     public function getProduct($id_category, $name_product)
     {
-        return response()->json([
-            'message' => 'Soy un API',
-            'param_1' => $id_category,
-            'param_2' => $name_product
-        ]);
+        if ($id_category == 0) {
+            if ($name_product == 'none') {
+                $message = 'todos los productos sin restriccion';
+                $data_products = DB::table('product')->paginate(16);
+            } else {
+                $message = 'todos los productos con la restriccion - '.$name_product;
+                $data_products = DB::table('product')
+                    ->where('name', 'LIKE', '%'.$name_product.'%')
+                    ->paginate(16);
+            }
+        } else {
+            if ($name_product == 'none') {
+                $message = 'productos de la categoria '.$id_category.' sin restriccion';
+                $data_products = DB::table('product')
+                    ->where('category', '=', $id_category)
+                    ->paginate(16);
+            } else {
+                $message = 'todos los productos de la categoria '.$id_category.' con la restriccion - '.$name_product;
+                $data_products = DB::table('product')
+                    ->where('category', '=', $id_category)
+                    ->where('name', 'LIKE', '%'.$name_product.'%')
+                    ->paginate(16);
+            }
+        }
+        return response()->json($data_products);
+        // TODO: Pendiente validar errores en la respuesta
     }
 }
